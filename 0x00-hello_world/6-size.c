b0a9de3173cf131c2eb4f7f0517c27435dd3d9cb
joethesaint <80671899+joethesaint@users.noreply.github.com>
2022-03-14 11:02:21 -0500

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *
+ * 
  * main - entry point
  * size of files
  * Return: Always 0 (Success)
@@ -12,5 +12,5 @@ printf("Size of an int: %d byte(s)\n", sizeof(int));
 printf("size of a long int: %d byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 printf("Size of a float: %d byte(s)\n", sizeof(float));
-return(0);
+return (0);
 }
