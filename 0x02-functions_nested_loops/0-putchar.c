10cb9d33a71f60b642c85f0251cc71b6a61c037d
joethesaint <80671899+joethesaint@users.noreply.github.com>
2022-06-08 19:06:44 -0500

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -20,31 +20,5 @@ _putchar('\n');
 
 return (0);
 }
-<<<<<<< HEAD
-
-#include "main.h"
 
 
-/**
- * main - Entry point
- *
- * Return: nothing, return void
- */
-
-int main(void)
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-
-return (0);
-}
-
-=======
->>>>>>> 2bae407f21d34314dcc8d66ae707fceb702b2ed8
