c7bdcdf92f5cdcaa48c53746fafe54ae5876dc19
joethesaint <joebamisaye068@gmail.com>
2022-06-20 14:57:41 -0500

    [fix]: testing

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,15 @@
 #include "main.h"
 /**
- *_memset -   fills memory with a constant byte
- *@s: pointer block of memory to fill
- *@b: value to set
- *@n: bytes of the memory
- *Return: dest
+ * _memset - fills memory with a constant byte
+ * @s: pointer block of memory to fill
+ * @b: value to set
+ * @n: bytes of the memory
+ * Return: the pointer to dest
+ *
+ * 	PSEUDOCODE	
+ * Loop through "s" using "n" as limit
+ * replace s[i] with "b"
+ * return s
  */
 
 char *_memset(char *s, char b, unsigned int n)
@@ -12,7 +17,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	/*Delacring FOR*/
+	/*Declaring FOR*/
 	for (i = 0; i < n; i++)
 	{
 		*(s + i) = b; /*add 1 position s*/
