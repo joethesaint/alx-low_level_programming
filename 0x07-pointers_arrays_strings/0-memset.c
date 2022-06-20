6d41f5d1e2b7fb172fd4ba9b02749ca721e30dbc
joethesaint <joebamisaye068@gmail.com>
2022-06-20 15:25:21 -0500

    [fix]: removed {} after for

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -19,10 +19,9 @@ char *_memset(char *s, char b, unsigned int n)
 
 	/*Declaring FOR*/
 	for (i = 0; i < n; i++)
-	{
 		*(s + i) = b; /*add 1 position s*/
 
-	} /*END FOR*/
+	 /*END FOR*/
 
 	return (s);
 }
