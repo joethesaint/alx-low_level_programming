adfef86fba5754c99ffd7d4f9488b31da665c54b
joethesaint <joebamisaye068@gmail.com>
2022-06-20 15:29:41 -0500

    [style]: commnts

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -17,11 +17,11 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	/*Declaring FOR*/
+	/** Declaring FOR*/
 	for (i = 0; i < n; i++)
-		*(s + i) = b; /*add 1 position s*/
+		*(s + i) = b; /** add 1 position s*/
 
-	 /*END FOR*/
+	 /** END FOR*/
 
 	return (s);
 }
