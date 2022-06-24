8af3e379857e67f9bf8b541c70de1eef5fccb0b3
joethesaint <joebamisaye068@gmail.com>
2022-06-24 17:53:32 -0500

    [feat]: add file 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: number of command line arguments
+ * @argv: array that contains the program command line arguments
+ */
 
 int main(int argc, char *argv[] __attribute__((unused)))
 {
