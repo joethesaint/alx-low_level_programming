077c1d0b3936b894f8709d6b4158d2c424d2cf12
joethesaint <joebamisaye068@gmail.com>
2022-06-24 18:06:15 -0500

    [fix]: betty style

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,18 +8,25 @@
 
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int i;
-
-	if (argc == 1)
-		printf("%d\n", argc - 1);
-	else
-	{
-		for (i = 0; *argv; i++, argv++)
-			;
-
-		printf("%d\n", argc - 1);
-	}
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
 
+
+/**
+ * int i;
+ *
+ *	if (argc == 1)
+ *		printf("%d\n", argc - 1);
+ *	else
+ *	{
+ *		for (i = 0; *argv; i++, argv++)
+ *			;
+ *
+ *		printf("%d\n", argc - 1);
+ *	}
+ *
+ *	return (0);
+ *
+ */
