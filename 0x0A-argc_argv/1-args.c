efee93264b0edaccc7eb3cd9d2e8fbe3d85c1a4e
joethesaint <joebamisaye068@gmail.com>
2022-06-24 17:57:38 -0500

    [feat]: add file 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,7 +8,18 @@
 
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc - 1);
+	int i;
+
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (i = 0; *argv; i++, argv++)
+			;
+
+		printf("%d\n", argc - 1);
+	}
+
 	return (0);
 }
 
