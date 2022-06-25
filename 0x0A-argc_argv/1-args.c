b108d09fd538c5e0309b85484685882b888c598c
joethesaint <joebamisaye068@gmail.com>
2022-06-24 21:31:27 -0500

    [feat]: add file 1 and 100

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,32 +1,12 @@
 #include <stdio.h>
-#include "main.h"
 /**
- * main - prints the number of arguments passed into it
- * @argc: number of command line arguments
- * @argv: array that contains the program command line arguments
+ * main - prints the number of arguments passed into it.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-
 int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
-
 	return (0);
 }
-
-
-/**
- * int i;
- *
- *	if (argc == 1)
- *		printf("%d\n", argc - 1);
- *	else
- *	{
- *		for (i = 0; *argv; i++, argv++)
- *			;
- *
- *		printf("%d\n", argc - 1);
- *	}
- *
- *	return (0);
- *
- */
