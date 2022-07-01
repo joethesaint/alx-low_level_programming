236e3d385df7f502ca97c0026059b08a7e1b631e
joethesaint <joebamisaye068@gmail.com>
2022-06-30 20:21:27 -0500

    fix code

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1_len++;
 
 	concat_len = s1_len + n;
-	
+
 	concat = malloc(sizeof(char) * (concat_len + 1));
 	if (concat == NULL)
 		return (NULL);
