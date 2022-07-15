4b1b9e212e11723f38bfbd0eef8dff76dca9255b
joethesaint <joebamisaye068@gmail.com>
2022-07-15 16:18:26 -0500

    [style]: rename elements to n

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,13 +10,13 @@
  */
 size_t list_len(const list_t *h)
 {
-	size_t elements = 0;
+	size_t n = 0;
 
 	while (h)
 	{
-		elements++;
+		n++;
 		h = h->next;
 	}
 
-	return (elements);
+	return (n);
 }
